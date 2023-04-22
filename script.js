2ad27b77ffc371a3e9063daccdf36c2809089518
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-04-23 00:25:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function firstNonRepeatedChar(str){
+	for (let index = 0; index < str.length; index++) {
+		if (str.indexOf(str.charAt(i))==str.lastIndexOf(str.charAt(i))) {
+			return str.charAt(i);
+		
+		}
+		
+	}
+}
+
+
+
+
+const str = prompt("Enter a string:");
+const result = firstNonRepeatedChar(str);
+console.log(result);
